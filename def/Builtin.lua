bb53d6cbf35e4dde458152c9880e1f6b3878a289
Jai <814683@qq.com>
2024-05-22 14:02:11 +0800

    feat: add builtin API compiletime

--- def/Builtin.lua
+++ def/Builtin.lua
@@ -1,3 +1,3 @@
----compiletime
+---compiletime Run it in compilation to get the result
 ---@type fun(func:fun():string|number|boolean|nil):string|number|boolean|nil
-function compiletime(func) end
+function _G.compiletime(func) end
